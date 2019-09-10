161f0279214f3136c7971ce1a0ecbc692b55c13a
steven nguyen <sqnguyen829@gmail.com>
2019-09-10 04:18:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -19,7 +19,21 @@ end
 
 
 
+def square_array(array)
+  
+  count=0
+  new_numbers=[]
+  while count < array.length do
 
+    new_numbers[count] = (Math.sqrt(array[count]))
+    
+    count +=1
+    
+  end
+  
+  new_numbers
+  
+end
 
 
 
